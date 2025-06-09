3131c314ccb070c8fe645b9a2c31d5d9d93df537
micha <michal.tusz12@gmail.com>
2025-06-09 14:01:15 +0200

    changed schedule response to include tasks

--- scheduleModule/serializers/daily_schedule_serializer.py
+++ scheduleModule/serializers/daily_schedule_serializer.py
@@ -1,9 +1,12 @@
 from rest_framework import serializers
 
 from scheduleModule.models import DailySchedule
+from scheduleModule.serializers.task_serializer import TaskSerializer
 
 
 class DailyScheduleSerializer(serializers.ModelSerializer):
+    tasks = TaskSerializer(many=True, read_only=True)
+
     class Meta:
         model = DailySchedule
-        fields = '__all__'
+        fields = ['id', 'Date', 'user', 'tasks']
